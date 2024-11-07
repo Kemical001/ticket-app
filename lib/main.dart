cf0581d5926046e0b504ecc8da3b3694a36bd512
Kemical001 <emmanueledet279@yahoo.com>
2024-11-07 08:12:27 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.cyan,
+            title: const Text("Welcome to flutter"),
+          ),
+          body: const Center(child: Text("Hello Flutter")),
+        ));
+  }
+}
