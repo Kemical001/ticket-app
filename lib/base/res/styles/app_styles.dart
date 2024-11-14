91e2c854b0becd1abc8910153c6ada5d8f0d7f35
Kemical001 <emmanueledet279@yahoo.com>
2024-11-14 13:37:51 +0100

    progress

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+Color primary = const Color(0xFF687daf);
+
+class AppStyles {
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0xFF3b3b3b);
+
+  static TextStyle headLineStyle1 =
+      TextStyle(fontSize: 25, fontWeight: FontWeight.w900, color: textColor);
+
+  static TextStyle headLineStyle3 =
+      const TextStyle(fontSize: 25, fontWeight: FontWeight.w500);
+}
