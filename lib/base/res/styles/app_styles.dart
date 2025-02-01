75420a753db38bf2dc74c26ce0e8b89db3e59450
Kemical001 <emmanueledet279@yahoo.com>
2025-02-01 16:05:32 +0100

    first commit

---
+++ lib/base/res/styles/app_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color(0xFF687daf);
+
+class AppStyles {
+  static Color primaryColor = primary;
+  static Color textColor = Color(0xFF3b3b3b);
+
+  static TextStyle headLineStyle1 = TextStyle(
+      fontSize: 25, fontWeight: FontWeight.w500, color: AppStyles.textColor);
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,10 +12,42 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         children: [
           Container(
-            child: Text("Good morning"),
-          ),
-          Container(
-            child: Text("You are welcome"),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good morning",
+                          style: TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Book Tickets",
+                          style: AppStyles.headLineStyle1,
+                        )
+                      ],
+                    ),
+                    Container(
+                      color: Color(0xFF526400),
+                      height: 50,
+                      width: 50,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Search Icon"), Text("Empty Space")],
+                )
+              ],
+            ),
           )
         ],
       ),
